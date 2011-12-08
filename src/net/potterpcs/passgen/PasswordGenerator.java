711808d8093ee52b5472568ad7f56d16449f50e1
Michael Potter <mpotter83@gmail.com>
2011-12-08 17:16:40 -0500

    Added comments

--- src/net/potterpcs/passgen/PasswordGenerator.java
+++ src/net/potterpcs/passgen/PasswordGenerator.java
@@ -19,7 +19,6 @@ import android.preference.PreferenceManager;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,18 +30,27 @@ public class PasswordGenerator extends Activity {
 	String possibleChars;
 	String genPassword;
 	Random rng;
-	
+
+	// This controls how fast the animation will be, and how long it will take.
+	// Longer passwords will usually take longer to animate.
+	static final long THREAD_SLEEP_MILLIS = 30;
+
+	// These are the minimum and maximum lengths for passwords. They are only
+	// used until the user sets a length in the preferences.
 	static final int MIN_PW_LENGTH = 6;
 	static final int MAX_PW_LENGTH = 10;
+
+	// This is the name of the file where passwords are saved. The least we can
+	// do is make it hidden until we can get some encryption or something.
+	static final String SAVED_PASSWORDS_FILE = ".saved.txt";	
+
+	// Bundle tags
 	static final String THREAD_PW_KEY = "password";
 	static final String THREAD_PW_CHARS_KEY = "numchars";
-	static final long THREAD_SLEEP_MILLIS = 30;
 	static final String PREFS_KEY_MINLENGTH = "minlength";
 	static final String PREFS_KEY_MAXLENGTH = "maxlength";
 	static final String PREFS_KEY_UPPERCASE = "uppercase";
 	static final String PREFS_KEY_NUMBERS = "numbers";
-	static final String SAVED_PASSWORDS_FILE = "saved.txt";
-	
 	
 	private TextView passwordText;
 	private PasswordSpinner passThread;
@@ -63,30 +71,25 @@ public class PasswordGenerator extends Activity {
         generatingText = (TextView) findViewById(R.id.generatingtext);
         passwordText = (TextView) findViewById(R.id.passwordtext);
 
-        if (oldPassword != null) {
-        	passwordText.setText(oldPassword, TextView.BufferType.SPANNABLE);
-        } else {
-        	skipButton.setVisibility(View.INVISIBLE);
-        	generatingText.setVisibility(View.INVISIBLE);
-        	passwordText.setVisibility(View.INVISIBLE);
-        	passwordText.setText(passwordText.getText(), TextView.BufferType.SPANNABLE);
-        }
+        skipButton.setVisibility(View.INVISIBLE);
+        generatingText.setVisibility(View.INVISIBLE);
+        passwordText.setVisibility(View.INVISIBLE);
+        passwordText.setText(passwordText.getText(), TextView.BufferType.SPANNABLE);
         
         rng = new SecureRandom();
+        
+        // This is our Handler; all it really does is update the password text
+        // with the latest "generated" password every few milliseconds.
+        // (Spoiler: When a character of the "generated" password matches the
+        // same position in the real password, it lights up in red and freezes.
+        // This serves no useful purpose, but it looks like something from
+        // WarGames, and that's really the whole point of the app.)
         
         passHandler = new Handler() {
         	public void handleMessage(Message msg) {
         		Bundle b = msg.getData();
         		String pw = b.getString(THREAD_PW_KEY);
-//        		int cs = b.getInt(THREAD_PW_CHARS_KEY);
         		SpannableString span = new SpannableString(pw);
-//        		span.setSpan(new ForegroundColorSpan(Color.RED), 0, cs, 
-//        				Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//        		
-//        		if (cs != pw.length()) {
-//	        		span.setSpan(new ForegroundColorSpan(Color.GRAY), cs, pw.length(),
-//	        				Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//        		}
         		for (int i = 0; i < pw.length(); ++i) {
         			span.setSpan(new ForegroundColorSpan(
         					pw.charAt(i) == genPassword.charAt(i) ? Color.RED : Color.GRAY),
@@ -122,6 +125,12 @@ public class PasswordGenerator extends Activity {
     }
     
     public void generatePassword(View v) {
+    	// This is where we actually generate a password, not just put on a show.
+    	// Basically, we figure out which characters are allowed, based on the
+    	// user's preferences. Next, we select a length, again between the min
+    	// and max from preferences. Then, we choose one of the allowed characters
+    	// for each position, and there you have it: a random password.
+    	
     	int max = Integer.parseInt(preferences.getString(PREFS_KEY_MAXLENGTH, Integer.toString(MAX_PW_LENGTH)));
     	int min = Integer.parseInt(preferences.getString(PREFS_KEY_MINLENGTH, Integer.toString(MIN_PW_LENGTH)));
     	int pwLength = min + rng.nextInt(max - min + 1);
@@ -132,21 +141,22 @@ public class PasswordGenerator extends Activity {
     	if (preferences.getBoolean(PREFS_KEY_NUMBERS, true)) {
     		pcs.append(resources.getString(R.string.numbers));
     	}
+    	// TODO: Add in symbols, etc.
+    	
     	possibleChars = pcs.toString();
     	char[] newPassword = new char[pwLength];
     	for (int i = 0; i < pwLength; ++i) {
     		char nextChar = possibleChars.charAt(rng.nextInt(possibleChars.length()));
     		newPassword[i] = nextChar;
-    		//    		passwordText.setText(new String(newPassword));
-    		//    		passwordText.setTextColor(Color.RED);
-    		//    		if (passwordText.getVisibility() != View.VISIBLE) {
-    		//    			passwordText.setVisibility(View.VISIBLE);
-    		//    		}
     	}
     	genPassword = new String(newPassword);
     	saved = false;
 
     	if (!threadAlive) {
+    		// Start up a thread to do the hard work of generating passwords.
+    		// We won't use any of these generated passwords for anything
+    		// except the movie-style animation.
+    		
     		passThread = new PasswordSpinner();
     		passThread.doSetup(passHandler, genPassword);
     		skipButton.setVisibility(View.VISIBLE);
@@ -165,6 +175,10 @@ public class PasswordGenerator extends Activity {
     }
     
     public void savePassword(View v) {
+    	// Save this password to the list.
+    	// TODO: The password list needs to be something more secure than a
+    	// plain text file.
+    	
     	FileOutputStream fos = null;
     	try {
     		if (!saved) {
@@ -195,7 +209,6 @@ public class PasswordGenerator extends Activity {
     private class PasswordSpinner extends Thread {
     	Handler handler;
     	String password;
-    	private static final String TAG = "PasswordSpinner";
 
 		public void doSetup(Handler h, String pw) {
 			handler = h;
@@ -204,7 +217,11 @@ public class PasswordGenerator extends Activity {
 
 		public void run() {
 			// This is to make a nice animation of "generating" the password,
-			// even though it's really already generated.
+			// even though it's really already generated. Sure, we *could*
+			// simply make the password and show it, but that's not really
+			// worthy of an app. (The user can skip the animation if they're
+			// in a hurry, though.)
+
 			if (password != null) {
 				char[] animatedPassword = new char[password.length()];
 				String apString = new String(animatedPassword);
@@ -218,15 +235,12 @@ public class PasswordGenerator extends Activity {
 						Message fm = handler.obtainMessage();
 						fm.setData(fb);
 						handler.sendMessage(fm);
-						Log.v(TAG, password);
 						return;
 					}
 
-					// One step of the animation
-//					for (int i = charsSoFar; i < animatedPassword.length; ++i) {
-//						char c = possibleChars.charAt(rng.nextInt(possibleChars.length()));
-//						animatedPassword[i] = c;
-//					}
+					// Animation Step: Generate a password, just like we did for
+					// the real one. We'll give that password to the Handler, and
+					// it will do the work of lighting up the correct characters.
 					charsSoFar = 0;
 					for (int i = 0; i < animatedPassword.length; ++i) {
 						if (animatedPassword[i] != password.charAt(i)) {
@@ -236,12 +250,6 @@ public class PasswordGenerator extends Activity {
 						}
 					}
 					apString = new String(animatedPassword);
-
-//					if (animatedPassword[charsSoFar] == password.charAt(charsSoFar)) {
-//						Log.v(TAG, Integer.toString(charsSoFar));
-//						Log.v(TAG, password + ", " + apString);
-//						++charsSoFar;
-//					}
 
 					if (password.contentEquals(apString)) {
 						threadAlive = false;
@@ -254,8 +262,6 @@ public class PasswordGenerator extends Activity {
 					Message m = handler.obtainMessage();
 					m.setData(b);
 					handler.sendMessage(m);
-
-					//					Log.v(TAG, apString);
 
 					try {
 						sleep(THREAD_SLEEP_MILLIS);
