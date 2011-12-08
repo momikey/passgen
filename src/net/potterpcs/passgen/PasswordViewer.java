b9371603180a652daf31b4c0effafc746d05fc05
Michael Potter <mpotter83@gmail.com>
2011-12-08 18:44:46 -0500

    Added license information

--- src/net/potterpcs/passgen/PasswordViewer.java
+++ src/net/potterpcs/passgen/PasswordViewer.java
@@ -1,3 +1,18 @@
+/*   Copyright 2011 Michael Potter
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package net.potterpcs.passgen;
 
 import java.io.FileInputStream;
@@ -9,11 +24,9 @@ import java.util.Arrays;
 
 import android.app.ListActivity;
 import android.content.Context;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Gravity;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
